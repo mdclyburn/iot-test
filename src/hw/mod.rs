06df476d763b531c1ed332a9733b9083ecfdc047
Marshall Clyburn <mardev2013@gmail.com>
2021-03-31 19:30:16 -0400

    Remove unused hardware code

--- src/hw/mod.rs
+++ src/hw/mod.rs
@@ -2,19 +2,6 @@
 
 use crate::facility::EnergyMetering;
 
-pub mod acs723;
 pub mod ina219;
-pub mod pcf8591;
-pub mod hal;
 
-pub use acs723::ACS723;
 pub use ina219::INA219;
-pub use pcf8591::PCF8591;
-
-use hal::ADCChannel;
-
-// impl<'a> EnergyMetering for (ADCChannel<'a>, ACS723) {
-//     fn current_draw(&self) -> u32 {
-//         0
-//     }
-// }
