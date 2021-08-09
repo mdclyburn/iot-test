7a5c1ba9abd46ce99a8b8ae02765e6643179b9f9
Marshall Clyburn <mardev2013@gmail.com>
2021-08-09 09:54:44 -0400

    Explicitly mark iterator-tied lifetime

--- common/src/input.rs
+++ common/src/input.rs
@@ -7,5 +7,5 @@ use crate::test::Test;
 /// Data adapter producing tests read from an input source.
 pub trait TestProvider: Debug {
     /// Create a Test-producing iterator.
-    fn tests(&self) -> Box<dyn Iterator<Item = Test> + '_>;
+    fn tests<'a>(&'a self) -> Box<dyn Iterator<Item = Test> + 'a>;
 }
