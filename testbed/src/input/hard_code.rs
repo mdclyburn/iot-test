d9a9f4fe94e346608232cd3a9bbfdcb13d7c4698
Marshall Clyburn <mardev2013@gmail.com>
2022-02-08 14:48:28 -0500

    Remove other sample hard-coded tests

--- testbed/src/input/hard_code.rs
+++ testbed/src/input/hard_code.rs
@@ -123,44 +123,12 @@ impl HardCodedTests {
     pub fn new() -> HardCodedTests {
         HardCodedTests {
             tests: vec![
-                // Test::new(
-                //     "radio-packet-tx",
-                //     (&["radio_send_app"]).into_iter().map(|x| *x),
-                //     (&[]).into_iter().copied(),
-                //     &[Operation::reset_device(),
-                //       Operation::idle_testbed(Duration::from_millis(5000))],
-                //     &[Criterion::Energy(EnergyCriterion::new("system-total", EnergyStat::Total)
-                //                         .with_max(350.0))]),
-
-                // Test::new(
-                //     "no-app-test",
-                //     (&[]).into_iter().map(|x| *x),
-                //     &[Operation { time: 0, pin_no: 23, input: Signal::Digital(false) },
-                //       Operation { time: 200, pin_no: 23, input: Signal::Digital(false) }],
-                //     &[Criterion::Energy(EnergyCriterion::new("system", EnergyStat::Average)
-                //                         .with_min(10.0))]),
-
-                // Test::new(
-                //     "blink-trace",
-                //     (&[]).into_iter().copied(),
-                //     (&["capsule/led/command/on", "capsule/led/command/off"]).into_iter().copied(),
-                //     &[Operation { time: 0, pin_no: 23, input: Signal::Digital(false) },
-                //       Operation { time: 3000, pin_no: 23, input: Signal::Digital(true) }],
-
                 Test::new(
-                    "serial-blink-trace",
+                    "empty-test",
                     (&[]).into_iter().copied(),
                     (&[]).into_iter().copied(),
                     &[Operation::at(0).idle_sync(Duration::from_millis(3000))],
-                    &[Criterion::SerialTrace(
-                        SerialTraceCriterion::new(&[
-                            SerialTraceCondition::new(&[0x6c, 0x65, 0x64, 0x20, 0x6f, 0x6e]),
-                            SerialTraceCondition::new(&[0x6c, 0x65, 0x64, 0x20, 0x6f, 0x6e])
-                                .with_timing(Timing::Relative(Duration::from_millis(250)),
-                                             Duration::from_millis(25)),
-                            SerialTraceCondition::new(&[0x6c, 0x65, 0x64, 0x20, 0x6f, 0x6e])
-                                .with_timing(Timing::Relative(Duration::from_millis(0)),
-                                             Duration::from_millis(10))]))],
+                    &[],
                     true),
             ],
         }
